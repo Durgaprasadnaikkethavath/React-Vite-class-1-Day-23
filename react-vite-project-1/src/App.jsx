57888f2e93ea5b9f711aaca18883303bc4ff6037
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-09-18 18:29:38 +0530

    first commit

---
+++ react-vite-project-1/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to React Vite class</h1>
+        <p>class : className</p>
+        <p>
+          One parent element for every write code. Example div tag are any other
+          tag can use parent tag
+        </p>
+        <h1>React Components</h1>
+        <p>
+          They are two type of Components 1.class Component 2.Function Component
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
